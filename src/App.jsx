0f40641d240d5d224ee6946c3d4bcbf85b2614a6
RefDev-Project <refdevproject@gmail.com>
2024-03-10 18:23:20 +0700

    useLayoutEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-// import UseReducer from "./Hooks/UseReducer";
 import UseEffect from "./Hooks/UseEffect";
+import UseLayoutEffect from "./Hooks/UseLayoutEffect";
 import UseReducer2 from "./Hooks/UseReducer2";
-// import UseState from "./Hooks/UseState";
+import UseState from "./Hooks/UseState";
 
 function App() {
   return (
     <>
+      <UseState />
       <UseReducer2 />
       <UseEffect />
+      <UseLayoutEffect />
     </>
   );
 }
---
+++ src/Hooks/UseLayoutEffect.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useLayoutEffect } from "react";
+import { useState } from "react";
+
+function UseLayoutEffect() {
+  const [count, setCount] = useState(0);
+
+  useLayoutEffect(() => {
+    console.log("console 1");
+  }, []);
+  useLayoutEffect(() => {
+    console.log("console 2");
+  }, []);
+
+  function handleIncrement() {
+    setCount((count) => count + 1);
+  }
+  return (
+    <>
+      {console.log("console 3")}
+      <button onClick={handleIncrement}>+</button>
+      <h1>{count}</h1>
+    </>
+  );
+}
+
+export default UseLayoutEffect;
